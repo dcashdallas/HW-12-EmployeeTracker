73ee8e37cba8263885ab26fa56fa7f1e60e76e94
dcashdallas <dcashdallas81@yahoo.com>
2020-06-20 13:24:18 -0500

    adding tables for department & role

--- db/eetracker_db.sql
+++ db/eetracker_db.sql
@@ -4,3 +4,23 @@ CREATE database eetracker_db;
 
 USE eetracker_db;
 
+CREATE TABLE department
+(
+    id INTEGER NOT NULL
+    auto_increment PRIMARY KEY,
+  name VARCHAR
+    (30)
+);
+
+    CREATE TABLE role
+    (
+        id INTEGER NOT NULL
+        auto_increment PRIMARY KEY,
+  title VARCHAR
+        (30),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY
+        (department_id) REFERENCES department
+        (id)
+);

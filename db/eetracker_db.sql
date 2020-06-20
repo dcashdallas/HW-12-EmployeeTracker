ca872077edbda069833c0b029e937aa4618a8000
dcashdallas <dcashdallas81@yahoo.com>
2020-06-20 13:37:32 -0500

    adding table for employee

--- db/eetracker_db.sql
+++ db/eetracker_db.sql
@@ -24,3 +24,21 @@ CREATE TABLE department
         (department_id) REFERENCES department
         (id)
 );
+
+        CREATE TABLE employee
+        (
+            id INTEGER NOT NULL
+            auto_increment PRIMARY KEY,
+  first_name VARCHAR
+            (30),
+  last_name VARCHAR
+            (30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY
+            (role_id) REFERENCES role
+            (id),
+  FOREIGN KEY
+            (manager_id) REFERENCES role
+            (id)
+);
